34b4bcb651f5a7daaf54b036116248d1bf5e8a69
natula <n.piatkowska@outlook.com>
2017-10-15 00:40:33 +0200

    project start and clean code excercise

---
+++ CleanThisCode.h
@@ -0,0 +1,18 @@
+#ifndef GITPROJECT_CLEANTHISCODE_H
+#define GITPROJECT_CLEANTHISCODE_H
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+void xks()
+
+{
+    int pqwemn5sdh; cout<<"provide number" ;cin>> pqwemn5sdh;     int  sdnf =   1, asnqw = 2;
+    while (asnqw <= sqrt(pqwemn5sdh) && sdnf != 0)          { sdnf = pqwemn5sdh % asnqw;
+        ++asnqw                                            ;}
+
+    if (sdnf == 0)         {
+        cout << "NO";} else {cout << "YES"                   ;}}
+
+
+#endif //GITPROJECT_CLEANTHISCODE_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "CleanThisCode.h"
+
+using namespace std;
+
+int main() {
+
+    xks();
+
+}
+
