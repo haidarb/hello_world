ce6b0feff9428587d3d6eb7aa41fd51bfa0fedcf
natula <n.piatkowska@outlook.com>
2017-10-21 10:37:30 +0200

    project start and clean code excercise

---
+++ ExchangeTwoValues.h
@@ -0,0 +1,30 @@
+#ifndef UNTITLED_EXCHANGETWOVALUES_H
+#define UNTITLED_EXCHANGETWOVALUES_H
+
+#include<iostream>
+
+using namespace std;
+
+void swapAB() {
+
+    int A, B, C;
+
+    cout << " Please provide A: " << endl;
+    cin >> A;
+    cout << " Please provide B: " << endl;
+    cin >> B;
+
+    cout << " Brilliant !" << endl;
+
+    C = A;
+    A = B;
+    B = C;
+
+    cout << " Now our A is equal to " << A << endl;
+    cout << ".. and B is equal to " << B << endl;
+
+
+
+}
+
+#endif //UNTITLED_EXCHANGETWOVALUES_H
